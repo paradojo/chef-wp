3f7bf107639389da308e02851ad0b52ad15cf9fb
Alan <alan@paradojo.com>
2016-04-17 12:09:23 -0500

    Update sync_from_master.rb

--- Deploy_software_wordpress_nginx-mysql56/recipes/sync_from_master.rb
+++ Deploy_software_wordpress_nginx-mysql56/recipes/sync_from_master.rb
@@ -1,19 +1,19 @@
-template "~/.ssh/elpinero-deploy-keys.pem" do
+template "/home/ubuntu/elpinero-deploy-keys.pem" do
  source "elpinero-deploy-keys.pem.erb"
  owner "root"
  group "root"
  mode "600"
 end
 
-file "~/git_wrapper.sh" do
+file "/home/ubuntu/git_wrapper.sh" do
   owner "root"
   mode "0755"
-  content "#!/bin/sh\nexec /usr/bin/ssh -i ~/.ssh/elpinero-deploy-keys.pem \"$@\""
+  content "#!/bin/sh\nexec /usr/bin/ssh -i /home/ubuntu/elpinero-deploy-keys.pem \"$@\""
 end
 
 git "/var/www/html" do
   repository "https://paradojo@bitbucket.org/paradojo/elpinero-wp.git"
   reference "master"
   action :sync
-  ssh_wrapper "~/git_wrapper.sh"
+  ssh_wrapper "/home/ubuntu//git_wrapper.sh"
 end
