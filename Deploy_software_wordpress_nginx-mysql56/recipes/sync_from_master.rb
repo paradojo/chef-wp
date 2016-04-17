0b6ecd5a315bae7971dcefc4305d456400695a42
Alan <alan@paradojo.com>
2016-04-17 14:03:49 -0500

    Update sync_from_master.rb

--- Deploy_software_wordpress_nginx-mysql56/recipes/sync_from_master.rb
+++ Deploy_software_wordpress_nginx-mysql56/recipes/sync_from_master.rb
@@ -1,6 +1,6 @@
 
 git "/var/www/html" do
-  repository "paradojo@bitbucket.org/paradojo/elpinero-wp.git"
+  repository "git@bitbucket.org:paradojo/elpinero-wp.git"
   revision "master"
   action :sync
   user "ubuntu"
