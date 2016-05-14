597c4d2f22a593502f9c05d198979b7c38e24ec6
Alan <alan@paradojo.com>
2016-05-13 22:49:43 -0500

    Update wp_permissions.rb

--- Deploy_software_wordpress_nginx-mysql56/recipes/wp_permissions.rb
+++ Deploy_software_wordpress_nginx-mysql56/recipes/wp_permissions.rb
@@ -1,4 +1,10 @@
-directory '/srv/www/wp/current' do
-    mode "0755"
-    recursive true
+#directory '/srv/www/wp/current' do
+#    mode "0755"
+#    recursive true
+#end
+
+node[:deploy].each do |application, deploy|
+  app_root = "#{deploy[:deploy_to]}/current"
+  execute "chmod -R g+rw #{app_root}" do
+  end
 end
